15603f15b34a7c37de201a7d40d46dc629a416fc
l_poncet <lloyd-dylan.poncet-davies@etu.u-pec.fr>
2026-04-01 11:28:27 +0200

    test

--- arbreBinaire.py
+++ arbreBinaire.py
@@ -1,6 +1,25 @@
-class Noeud():
-    __init__(self, v, g, d):
+class NoeudB():
+    def __init__(self, v, g, d):
         self.v=v
         self.g=g
         self.d=d
-    
+    def __str__(self):
+        
+    def get_v(self):
+        return self.v
+    def get_g(self):
+        return g
+    def get_d(self):
+        return d
+    def arbre_gauche(self):
+        return self.g!=None
+    def arbre_droit(self):
+        return self.d!=None
+    def arbre_f(self):
+        return self.g==None and self.d==None
+    def arbre_v(self):
+        return self.v==None
+    def hauteur(self):
+        if arbre_v():
+            return 0
+        return 1+max(hauteur(self.g), hauteur(self.d))
