f5bccf67e80d54e1799062ab8e77e36bfd39a80f
l_poncet <lloyd-dylan.poncet-davies@etu.u-pec.fr>
2026-04-02 16:41:03 +0200

    Résolution de problème

--- NoeudHuffman.py
+++ NoeudHuffman.py
@@ -26,6 +26,7 @@ class NoeudHuffman(NoeudBinaire):
                 encodage += "1"
                 return self.get_d().encodage_huffman(lettre,encodage)
     
+    @staticmethod
     def compression(texte,liste_encodage):
         resultat = ""
         for chr in texte:
@@ -35,28 +36,43 @@ class NoeudHuffman(NoeudBinaire):
         return resultat
     
     @staticmethod
+    def tri_noeuds(liste_noeud):
+        """Trie une liste d'objets NoeudHuffman par ordre décroissant de poids."""
+        for i in range(len(liste_noeud)):
+            maxi = i
+            for j in range(i + 1, len(liste_noeud)):
+                poids_maxi = liste_noeud[maxi].get_v()[1]
+                poids_actuel = liste_noeud[j].get_v()[1]
+                
+                if poids_maxi < poids_actuel:
+                    maxi = j
+                    
+            temp = liste_noeud[i]
+            liste_noeud[i] = liste_noeud[maxi]
+            liste_noeud[maxi] = temp
+
+    @staticmethod
     def construction_arbre(table_effectif):
-    #On trie la table d'effectif par ordre décroissant
-    table_effectif_trie = tri_tab(table_effectif)
-    liste_noeud = []
-    for i in table_effectif_trie:
-        liste_noeud.append(NoeudHuffman(i,None,None))
-     
-    while len(liste_noeud) > 1:
-        noeud_gauche = liste_noeud.pop()
-        noeud_droit = liste_noeud.pop()
+        #On trie la table d'effectif par ordre décroissant
+        liste_noeud = []
+        for i in table_effectif:
+            liste_noeud.append(NoeudHuffman(i,None,None))
+        
+        while len(liste_noeud) > 1:
+            noeud_gauche = liste_noeud.pop()
+            noeud_droit = liste_noeud.pop()
 
-        tuple_g = noeud_gauche.get_v()
-        tuple_d = noeud_droit.get_v()
+            tuple_g = noeud_gauche.get_v()
+            tuple_d = noeud_droit.get_v()
 
-        nouveau_t = (tuple_g[0] + tuple_d[0], tuple_g[1] + tuple_d[1])
+            nouveau_t = (tuple_g[0] + tuple_d[0], tuple_g[1] + tuple_d[1])
 
-        nouveau_noeud = NoeudHuffman(nouveau_t,noeud_gauche,noeud_droit)
-        liste_noeud.append(nouveau_noeud)
+            nouveau_noeud = NoeudHuffman(nouveau_t,noeud_gauche,noeud_droit)
+            liste_noeud.append(nouveau_noeud)
 
-        tri_noeuds(liste_noeud)
-      
-    return liste_noeud[0]
+            NoeudHuffman.tri_noeuds(liste_noeud)
+        
+        return liste_noeud[0]
 
 
         
